289c90d8edd3df29e275de667d28570bd8675147
Global Username <globalemail@example.com>
2025-12-04 23:00:02 +0500

    fourth

--- config.py
+++ config.py
@@ -1,7 +1,18 @@
 import os
 
 class Config:
+    # Секретный ключ для сессий и flash
     SECRET_KEY = os.environ.get('SECRET_KEY', 'supersecretkey123')
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')  # Render передаст URL базы
+
+    # URL для подключения к PostgreSQL
+    # Если на Render есть DATABASE_URL, он её использует, иначе дефолт
+    SQLALCHEMY_DATABASE_URI = os.environ.get(
+        'DATABASE_URL',
+        'postgresql://postgres1:kBztkPjG32gKTCMfBvHwVTGTQ7Xms5Il@dpg-d4osgdc9c44c738fuod0-a/mektep_db_9ybu'
+    )
+
+    # Отключаем уведомления SQLAlchemy об изменениях
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+    # Папка для загрузки фото учителей
     UPLOAD_FOLDER = os.path.join('static', 'uploads')
